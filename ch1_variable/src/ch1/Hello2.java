50b01c6f1a30e2043b7de131ccf53abe7422a8bc
bbibbi7683 <bbibbi7683@gmail.com>
2024-02-15 20:35:36 +0900

    클래스 작성

---
+++ ch1_variable/src/ch1/Hello2.java
@@ -0,0 +1,14 @@
+package ch1;
+
+// 클래스 이름은 대문자로 시작
+// 클래스 명은 한글로 가능하나, 영어로 사용
+
+public class Hello2 {
+    public static void main(String[] args) {
+        int var1 = 5;
+        System.out.println("내가 좋아하는 숫자는" + var1);
+      
+        
+    }
+
+}
